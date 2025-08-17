7ebc7e8ab7ff24066ee1b798ff4df5a798c24feb
BalaMeena2205 <balameenaaseenivasan@gmail.com>
2025-08-17 12:38:27 +0530

    First commit

---
+++ Letgen/src/App.jsx
@@ -0,0 +1,19 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';  
+import Signup from './Signup';  
+import Login from './Login';  
+import Dashboard from './Dashboard';
+
+function App() {  
+  return (  
+    <BrowserRouter>  
+      <Routes>  
+        <Route path='/' element={<Login/>}/>
+        <Route path='/register' element={<Signup />} />  
+        <Route path='/login' element={<Login />} />
+        <Route path='/dashboard' element={<Dashboard />} />  
+      </Routes>  
+    </BrowserRouter>  
+  );  
+}  
+
+export default App;  
